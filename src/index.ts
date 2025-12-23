d64a361435746c19476b74a8af923a4b54d4da94
Damir <damircodenn@gmail.com>
2025-12-23 19:31:50 +0500

    Database Setup & Error Handling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { config } from './config/app.config';
+import connectDatabase from './database/models/database';
+import { errorHandler } from './middlewares/errorHandler';
+import { HTTPSTATUS } from './config/http.config';
+import { asyncHandler } from './middlewares/asyncHandler';
 
 const app = express();
 // const BASE_PATH = config.BASE_PATH;
@@ -18,12 +22,15 @@ app.use(
 
 app.use(cookieParser());
 
-app.get('/', (req: Request, res: Response) => {
-	res.status(200).json({
+app.get('/', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+	res.status(HTTPSTATUS.OK).json({
 		message: "Hello Subscribers!!!",
 	})
-})
+}));
+
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
 	console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+	await connectDatabase();
 });
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,25 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { HTTPSTATUS } from '../config/http.config';
+import { AppError } from '../common/utils/AppError';
+
+export const errorHandler: ErrorRequestHandler = (error: Error, req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> => {
+	console.error(`Error occured on PATH: ${req.path}`, error);
+
+	if (error instanceof SyntaxError) {
+		return res.status(HTTPSTATUS.BAD_REQUEST).json({
+			message: 'Invalid JSON format, please check your request body'
+		});
+	}
+
+	if (error instanceof AppError) {
+		return res.status(error.statusCode).json({
+			message: error.message,
+			errorCode: error.errorCode
+		});
+	}
+
+	return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+		message: 'Internal Server Error',
+		error: error?.message || 'Unknown error occured'
+	});
+}
